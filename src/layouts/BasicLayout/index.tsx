d15295f3e15c282997d710a422fe2959a4850c51
hocgin <hocgin@gmail.com>
2022-05-09 18:00:02 +0800

    qiankun

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,9 +1,41 @@
 import styles from './index.less';
 import React from 'react';
+import {Layout, Menu} from "antd";
+import {Link, useLocation} from "umi";
 
 const BasicLayout: React.FC<{}> = (props, ref) => {
+  let {base}: any = props;
+  const selectKey = '/' + useLocation().pathname.split('/')[1];
+
   return (<div className={styles.normal}>
-    <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+    <Layout.Header>
+      <div className={styles.logo}>{base?.name}</div>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['home']}
+        selectedKeys={[selectKey]}
+        style={{lineHeight: '64px'} as any}
+      >
+        <Menu.Item key="/">
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        {base?.apps.map((app: any, index: number) => {
+          if (index === 2) {
+            return (
+              <Menu.Item key={app.base}>
+                <Link to="/app3/123">{app.name}</Link>
+              </Menu.Item>
+            );
+          }
+          return (
+            <Menu.Item key={app.base}>
+              <Link to={app.base}>{app.name}</Link>
+            </Menu.Item>
+          );
+        })}
+      </Menu>
+    </Layout.Header>
     {props.children}
   </div>);
 };
