43d3fccaf47a5178717cc4f1fa05362120555739
hocgin <hocgin@gmail.com>
2022-06-28 10:25:53 +0800

    调整

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -3,27 +3,48 @@ import React from 'react';
 import {Layout, Menu} from "antd";
 import {Link, useLocation} from "umi";
 import {apps} from '@/qiankun';
+import {useToggle} from "ahooks";
+import classnames from "classnames";
+
+const SlaveMenu: React.FC<{ items?: any[], selectKey?: string, theme?: string }> = ({
+                                                                                      items = [],
+                                                                                      selectKey,
+                                                                                      theme = 'dark'
+                                                                                    }) => {
+  let [show, {toggle}] = useToggle(false);
+
+  return (<Layout.Header className={classnames(styles.slaveMenu, {
+    [styles.hidden]: !show,
+  })}>
+    <div className={styles.container}>
+      <div className={styles.logo}/>
+      <Menu theme={theme as any}
+            mode="horizontal"
+            defaultSelectedKeys={['home']}
+            selectedKeys={[selectKey]}
+            style={{lineHeight: '64px'} as any}
+            items={items}/>
+    </div>
+    <button
+      className={classnames(styles.toggle, {
+        [styles.dark]: theme === 'dark',
+      })}
+      onClick={toggle}>
+      {show ? '-' : '+'}
+    </button>
+  </Layout.Header>)
+};
 
 
 const BasicLayout: React.FC<{}> = (props, ref) => {
-  console.log('props', apps);
   const selectKey = '/' + useLocation().pathname.split('/')[1];
+  let menus = [{name: 'Home'}, ...apps].map((app: any, index) => ({
+    key: index,
+    label: <Link to={`/${app.name}`}>{app.name}</Link>
+  }));
 
   return (<Layout className={styles.normal}>
-    <Layout.Header>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['home']}
-        selectedKeys={[selectKey]}
-        style={{lineHeight: '64px'} as any}
-        items={[{name: '/'}, ...apps].map((app: any, index) => ({
-          key: index,
-          label: <Link to={`/${app.name}`}>{app.name}</Link>
-        }))}
-      >
-      </Menu>
-    </Layout.Header>
+    <SlaveMenu items={menus} selectKey={selectKey}/>
     <Layout.Content>{props.children}</Layout.Content>
   </Layout>);
 };
